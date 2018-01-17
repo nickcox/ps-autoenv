0f7d34daa5b3d7d308369570ebf11330a5d11621
Nick Cox <ncox@live.com>
2018-01-17 21:30:52 +1100

    init

---
+++ about_PS-Autoenv.help.txt
@@ -0,0 +1,39 @@
+PowerShell implementation of [Autoenv](https://github.com/kennethreitz/autoenv).
+
+What is it?
+===========
+
+If a directory contains a .autoenv file, it will automatically be executed when you cd into it. When enabled
+(set $autoenv:ENABLE_LEAVE to $true), if a directory contains a .autoenv.leave file, it will automatically be executed when you leave it.
+
+This is great for...
+
+* auto-activating virtualenvs
+* auto-deactivating virtualenvs
+* project-specific environment variables
+
+
+Usage
+==========
+```
+PS> Import-Module ps-autoenv
+
+$ echo "echo 'whoa'" > project/.autoenv
+$ cd project
+whoa
+```
+
+Install
+==========
+Install-Module ps-autoenv
+Add-Content $PROFILE @("`n", "import-module ps-autoenv")
+
+Configuration
+============
+Update these properties of $autoenv at any time:
+
+AUTH_FILE: Contains a list of authorized env files, defaults to ~/.autoenv_authorized
+ENV_FILENAME: Name of the .env file, defaults to .autoenv
+ENV_LEAVE_FILENAME: Name of the .env.leave file, defaults to .autoenv.leave
+ENABLE_LEAVE: Set this to a non-falsy value in order to enable leave scripts
+ASSUME_YES: Set this variable to automatically authorize the initialization of new environments
---
+++ ps-autoenv.psm1
@@ -0,0 +1,101 @@
+Set-StrictMode -Version latest
+
+$script:currentDir = $pwd
+$script:currentEnvDir = $null
+
+$global:autoenv = New-Object PSObject -Property ([ordered]@{
+  AUTH_FILE = '~/.autoenv_authorized'
+  ENV_FILENAME = '.autoenv'
+  ENV_LEAVE_FILENAME = '.autoenv.leave'
+  ENABLE_LEAVE = $true
+  ASSUME_YES = $false
+})
+
+function AuthorizeFile($filePath) {
+  if (-not (Test-Path $autoenv.AUTH_FILE)) {
+    New-Item $autoenv.AUTH_FILE
+  }
+
+  if ((Get-Content $autoenv.AUTH_FILE) -contains $filePath) {
+    return $true
+  }
+
+  Write-Warning 'PS-AutoEnv wants to authorize the following script:'
+  Write-Host ('=' * 60) -ForegroundColor Red
+  Get-Content $filePath | Write-Host -ForegroundColor Green
+  Write-Host ('=' * 60) -ForegroundColor Red
+
+  if ($autoenv.ASSUME_YES) {
+    Write-Host "$([char]0x2713) Auto authorized `n" -ForegroundColor DarkYellow
+    $filePath >> $autoenv.AUTH_FILE
+    return $true
+  }
+
+  switch (Read-Host "Authorize file ($filePath) ( y / n )") {
+    "y" {
+      $filePath >> $autoenv.AUTH_FILE
+      return $true
+    }
+    Default {
+      return $false
+    }
+  }
+}
+
+function RunScript($scriptFile) {
+  if (AuthorizeFile $scriptFile.FullName) {
+    Write-Verbose "Running script: $scriptFile"
+
+    #Set $PSScriptRoot for convenience
+    $block = "param (`$PSScriptRoot)`n" + (Get-Content $scriptFile.FullName -Raw)
+    $output = Invoke-Command `
+      -ScriptBlock ([scriptblock]::Create(($block))) `
+      -ArgumentList $scriptFile.DirectoryName
+    Write-Host $output
+  }
+}
+
+function LeaveDirectory($dir) {
+  Write-Verbose "Leaving directory: $dir"
+  if (
+    $autoenv.ENABLE_LEAVE -and (
+    $leaveFile = Get-ChildItem $dir $autoenv.ENV_LEAVE_FILENAME)) {
+    RunScript $leaveFile
+    $script:currentEnvDir = $null
+  }
+}
+
+function EnterDirectory($dir) {
+  Write-Verbose "Entered directory: $dir"
+  if ($enterFile = Get-ChildItem $dir $autoenv.ENV_FILENAME) {
+    RunScript $enterFile
+    $script:currentEnvDir = $dir
+  }
+}
+
+function AutoEnv($dir) {
+  try {
+    if ($dir -eq $currentDir) {
+      return
+    }
+
+    LeaveDirectory $currentDir
+    EnterDirectory $dir
+
+    $script:currentDir = $dir
+  }
+  catch {
+   Write-Warninging $_.Exception.Message
+  }
+}
+
+$validateAttr = (new-object ValidateScript { AutoEnv $_; return $true })
+
+(Get-Variable pwd).attributes.Add($validateAttr)
+
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
+  $null = (Get-Variable pwd).attributes.Remove($validateAttr)
+  $global:autoenv = $null
+}
+
+Export-ModuleMember -Variable $autoenv
